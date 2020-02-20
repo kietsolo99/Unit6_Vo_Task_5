06c6f2ba8f19de8603574525d5b515967f961897
kietsolo99 <kiet.vo054@my.tccd.edu>
2020-02-20 03:16:48 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "accounts.h"
+#include <algorithm> //to use the sort command
+using namespace std;
+
+void printArray(int[], int);
+void sortArrDesc(int[], int);
+int main() 
+{
+  //using build-in sort Asc to sort an array
+  cout << "\nStart: " << cpuTime() << endl;
+  sort(accountBalances, accountBalances+maxAccounts);
+  printArray(accountBalances, maxAccounts);
+  //build-in reverse
+  reverse(accountBalances, accountBalances+maxAccounts);
+  printArray(accountBalances, maxAccounts);
+  cout << "\nEnd: " << cpuTime() << endl;
+  return 0;
+}
+
+
+//Funtion definition to Print Array
+void printArray(int arrayVals[], int size)
+{
+  cout <<"\nPrinted Values in Array: " << endl;
+  for(int i = 0; i < size; i++)
+  {
+    cout << arrayVals[i] << ",";
+  }
+
+}
